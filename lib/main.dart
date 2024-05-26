b770a59e13d7e9f4f6e76333615060d7b7ebcfde
chotixity <melkychoti@gmail.com>
2024-05-26 13:24:19 +0300

    Responsiveness on mobile screens

--- lib/main.dart
+++ lib/main.dart
@@ -53,219 +53,232 @@ class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   elevation: 5,
-      //   backgroundColor: Colors.blue,
-      //   leading: const Icon(
-      //     Icons.headset,
-      //     size: 50,
-      //   ),
-      //   title: const Text("Listen all you want"),
-      //   centerTitle: true,
-      //   actions: [
-      //     IconButton(
-      //       onPressed: () {},
-      //       icon: const Icon(
-      //         Icons.settings,
-      //         size: 50,
-      //       ),
-      //     ),
-      //   ],
-      // ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              // padding: const EdgeInsets.only(left: 20),
-              width: MediaQuery.sizeOf(context).width * 0.25,
-              height: MediaQuery.sizeOf(context).height * 0.95,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.onPrimaryContainer,
-                borderRadius: const BorderRadiusDirectional.only(
-                  topEnd: Radius.circular(15),
-                  topStart: Radius.circular(15),
-                ),
-              ),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 120,
-                    child: Card(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+      appBar: MediaQuery.sizeOf(context).width < 600
+          ? AppBar(
+              elevation: 5,
+              backgroundColor: Colors.blue,
+              title: Wrap(
+                spacing: 5,
+                children: List.generate(
+                  4,
+                  (index) {
+                    return ChoiceChip(
+                      padding: EdgeInsets.zero,
+                      label: Text(
+                        labels[index],
+                        style: Theme.of(context).textTheme.bodySmall,
                       ),
-                      elevation: 5,
-                      color: Theme.of(context).colorScheme.secondary,
-                      child: Padding(
-                        padding: const EdgeInsets.all(0.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Icon(
-                                  Icons.home_filled,
-                                  size: 50,
-                                  color:
-                                      Theme.of(context).colorScheme.onSecondary,
-                                ),
-                                const Text(
-                                  "search",
-                                  //style: Theme.of(context).textTheme.bodyLarge,
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Icon(
-                                  Icons.search_outlined,
-                                  size: 50,
-                                  color:
-                                      Theme.of(context).colorScheme.onSecondary,
-                                ),
-                                Text(
-                                  "Home",
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                      child: Card(
-                    elevation: 5,
-                    color: Theme.of(context).colorScheme.secondary,
-                    child: Column(
-                      children: [
-                        ListTile(
-                          leading: Icon(
-                            Icons.library_books,
-                            color: Theme.of(context).colorScheme.onSecondary,
-                          ),
-                          title: Text(
-                            "Your Library",
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                          trailing: SizedBox(
-                            width: 100,
-                            child: Row(
-                              children: [
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.add,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onSecondary,
-                                  ),
-                                ),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.arrow_forward,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onSecondary,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Wrap(
-                          spacing: 5,
-                          children: List.generate(
-                            4,
-                            (index) {
-                              return ChoiceChip(
-                                label: Text(
-                                  labels[index],
-                                  style: Theme.of(context).textTheme.bodySmall,
-                                ),
-                                selected: _value == index,
-                                onSelected: (bool selected) {
-                                  setState(() {
-                                    _value = selected ? index : null;
-                                  });
-                                },
-                              );
-                            },
-                          ).toList(),
-                        ),
-                        Expanded(
-                          child: ListView(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      Icons.search,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSecondary,
-                                    ),
-                                  ),
-                                  TextButton.icon(
-                                    iconAlignment: IconAlignment.end,
-                                    onPressed: () {},
-                                    label: Text(
-                                      "Recently Added",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium,
-                                    ),
-                                    icon: Icon(
-                                      Icons.list,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSecondary,
-                                    ),
-                                  )
-                                ],
-                              ),
-                              RecentsList(items),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  )),
-                ],
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.sizeOf(context).longestSide * .02,
-            ),
-            Expanded(
-              child: Container(
-                height: MediaQuery.sizeOf(context).height * .95,
-                color: Theme.of(context).colorScheme.onPrimaryContainer,
-                child: Column(
-                  children: [
-                    SizedBox(
-                        height: MediaQuery.sizeOf(context).height * .3,
-                        child: RecentsGrid(items))
-                  ],
-                ),
+                      selected: _value == index,
+                      onSelected: (bool selected) {
+                        setState(() {
+                          _value = selected ? index : null;
+                        });
+                      },
+                    );
+                  },
+                ).toList(),
               ),
             )
-          ],
-        ),
+          : null,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            return _mobileView();
+          } else {
+            return _wideScreens();
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  Widget _wideScreens() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          // padding: const EdgeInsets.only(left: 20),
+          width: MediaQuery.sizeOf(context).width * 0.25,
+          height: MediaQuery.sizeOf(context).height * 0.95,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.onPrimaryContainer,
+            borderRadius: const BorderRadiusDirectional.only(
+              topEnd: Radius.circular(15),
+              topStart: Radius.circular(15),
+            ),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 120,
+                child: Card(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  elevation: 5,
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.home_filled,
+                              size: 50,
+                              color: Theme.of(context).colorScheme.onSecondary,
+                            ),
+                            const Text(
+                              "search",
+                              //style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.search_outlined,
+                              size: 50,
+                              color: Theme.of(context).colorScheme.onSecondary,
+                            ),
+                            Text(
+                              "Home",
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: Card(
+                elevation: 5,
+                color: Theme.of(context).colorScheme.secondary,
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.library_books,
+                        color: Theme.of(context).colorScheme.onSecondary,
+                      ),
+                      title: Text(
+                        "Your Library",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.add,
+                                color:
+                                    Theme.of(context).colorScheme.onSecondary,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.arrow_forward,
+                                color:
+                                    Theme.of(context).colorScheme.onSecondary,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Wrap(
+                      spacing: 5,
+                      children: List.generate(
+                        4,
+                        (index) {
+                          return ChoiceChip(
+                            label: Text(
+                              labels[index],
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                            selected: _value == index,
+                            onSelected: (bool selected) {
+                              setState(() {
+                                _value = selected ? index : null;
+                              });
+                            },
+                          );
+                        },
+                      ).toList(),
+                    ),
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.search,
+                                  color:
+                                      Theme.of(context).colorScheme.onSecondary,
+                                ),
+                              ),
+                              TextButton.icon(
+                                iconAlignment: IconAlignment.end,
+                                onPressed: () {},
+                                label: Text(
+                                  "Recently Added",
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                ),
+                                icon: Icon(
+                                  Icons.list,
+                                  color:
+                                      Theme.of(context).colorScheme.onSecondary,
+                                ),
+                              )
+                            ],
+                          ),
+                          RecentsList(items),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )),
+            ],
+          ),
+        ),
+        SizedBox(
+          width: MediaQuery.sizeOf(context).longestSide * .02,
+        ),
+        Expanded(
+          child: Container(
+            height: MediaQuery.sizeOf(context).height * .95,
+            color: Theme.of(context).colorScheme.onPrimaryContainer,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * .3,
+                  child: RecentsGrid(items),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _mobileView() {
+    return RecentsGrid(items);
   }
 }
--- lib/widgets/recents_grid.dart
+++ lib/widgets/recents_grid.dart
@@ -6,46 +6,72 @@ class RecentsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      //restorationId: ,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // Determine the maxCrossAxisExtent dynamically
+        double maxCrossAxisExtent = constraints.maxWidth / 2;
+        bool responsive = constraints.maxWidth < 600;
+        double childAspectRatio = constraints.maxWidth < 600 ? 2.5 : (1 / .2);
 
-      padding: const EdgeInsets.all(8.0),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 480,
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 8,
-        childAspectRatio: (1 / .2),
-      ),
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return Card.filled(
-          //semanticContainer: ,
-          //clipBehavior: Clip.antiAliasWithSaveLayer,
-          child: Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Row(
-              children: [
-                Image.asset(
-                  items[index]["image"],
-                ),
-                const SizedBox(width: 10),
-                Text(
-                  "Daily Mix ${index + 1}",
-                  style: const TextStyle(color: Colors.black),
-                ),
-                const Expanded(child: SizedBox()),
-                IconButton(
-                  tooltip: "Play this music",
-                  hoverColor: Colors.green,
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.play_arrow,
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            ),
+        return GridView.builder(
+          //padding: const EdgeInsets.all(8.0),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: maxCrossAxisExtent,
+            mainAxisSpacing: responsive ? 4 : 8,
+            crossAxisSpacing: responsive ? 2 : 8,
+            childAspectRatio: childAspectRatio,
           ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Card(
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: Padding(
+                padding: constraints.maxWidth < 600
+                    ? EdgeInsets.zero
+                    : const EdgeInsets.only(right: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      //scale: re
+                      items[index]["image"],
+                      width: responsive ? 60 : null,
+                      fit: BoxFit.cover,
+                      height: double.infinity,
+                    ),
+                    constraints.maxWidth < 600
+                        ? const SizedBox(width: 10)
+                        : const SizedBox(
+                            width: 0,
+                          ),
+                    Text(
+                      "Daily Mix ${index + 1}",
+                      style: const TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    constraints.maxWidth < 600
+                        ? const SizedBox(width: 10)
+                        : const Expanded(
+                            child: SizedBox(
+                                //width: 0,
+                                ),
+                          ),
+                    IconButton(
+                      tooltip: "Play this music",
+                      hoverColor: Colors.green,
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.play_arrow,
+                        color: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
         );
       },
     );
